d92161ddf39e8b7c5ea1a50ae732d067c8c5cca0
the-shuvalovan <88106763+the-shuvalovan@users.noreply.github.com>
2021-08-13 12:28:53 +0300

    Delete task_2.py

--- task_2.py
+++
@@ -1,8 +0,0 @@
-number = int(input('Введите время в секундах: '))
-hours = number // 3600
-sec = number % 3600
-minutes = sec // 60
-seconds = sec % 60
-
-print (hours, minutes, seconds)
-print("{:02d}:{:02d}:{:02d}".format(hours, minutes, seconds))
