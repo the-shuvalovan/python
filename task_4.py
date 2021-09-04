5cd3de9b14c99f9683268d3d5c6214c4ae29cf78
the-shuvalovan <88106763+the-shuvalovan@users.noreply.github.com>
2021-09-04 16:05:49 +0300

    Delete task_4.py

--- task_4.py
+++
@@ -1,7 +0,0 @@
-number=int(input('Введите целое положительное число: '))
-result=0
-while number > 0:
-    i = number % 10
-    if i >= result: result=i
-    number //= 10
-print(f"Самая большая цифра в вашем числе - {result}")
